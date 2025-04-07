fb1d1e6a34ff9c22e216dd167c5565538b7e4e8d
Alejandro <alejandrogrisalesvelez@gmail.com>
2025-04-07 17:32:50 -0500

    mi tercer commit

---
+++ proyects/hola-mundo/src/App.jsx
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/dribbble/omidnikrah" alt="El avatar" />
+        <div>
+          <strong>Migler Ángel Durán</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
